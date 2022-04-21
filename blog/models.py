f65278644dbff3b4d72f82bf7da01496fcc34287
Tomasz Sagan <w62950@student.wsiz.edu.pl>
2022-04-21 15:08:18 +0200

    dodanie nowych plików i szablonów html

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
-    title = models.CharField(max_length = 200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default = timezone.now)
-    publish_date = models.DateTimeField(blank = True, null = True)
+    create_date = models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.publish_date = timezone.now()
